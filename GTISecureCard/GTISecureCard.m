ae5610e5f2e708d8d3cf05d035a4f2c6342ce8e1
Quan Pham <doraemonfanclub@gmail.com>
2019-05-03 18:19:40 +0700

    Initial Commit

---
+++ GTISecureCard/GTISecureCard.m
@@ -0,0 +1,13 @@
+//
+//  GTISecureCard.m
+//  GTISecureCard
+//
+//  Created by Quan Pham on 5/3/19.
+//  Copyright © 2019 Quan Pham. All rights reserved.
+//
+
+#import "GTISecureCard.h"
+
+@implementation GTISecureCard
+
+@end
